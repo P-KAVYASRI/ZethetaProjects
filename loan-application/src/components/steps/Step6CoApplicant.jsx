04f08f6388cd4a1c50c84bf6e337da981e5a08bb
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-15 13:10:45 +0530

    step6CoApplicant

---
+++ loan-application/src/components/steps/Step6CoApplicant.jsx
@@ -0,0 +1,13 @@
+function Step6CoApplicant() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Step 6 - Co Applicant
+      </h2>
+
+      <p>Co applicant form here</p>
+    </div>
+  );
+}
+
+export default Step6CoApplicant;
