055f9172de26053e9905b2b3dc46c318ebefd632
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-15 13:12:04 +0530

    Finally step8

---
+++ loan-application/src/components/steps/Step8Review.jsx
@@ -0,0 +1,13 @@
+function Step8Review() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Step 8 - Review & Submit
+      </h2>
+
+      <p>Review form here</p>
+    </div>
+  );
+}
+
+export default Step8Review;
