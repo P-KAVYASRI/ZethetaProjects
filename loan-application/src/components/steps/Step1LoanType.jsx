6059f5108a23ae3c1e92d053a8f069a4383ec4d9
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-15 13:05:51 +0530

    step1loanType

---
+++ loan-application/src/components/steps/Step1LoanType.jsx
@@ -0,0 +1,13 @@
+function Step1LoanType() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Step 1 - Loan Type
+      </h2>
+
+      <p>Loan form step content here</p>
+    </div>
+  );
+}
+
+export default Step1LoanType;
