bda85100b16be6376d02652da859b238ed1ad435
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-15 13:09:16 +0530

    step4Address

---
+++ loan-application/src/components/steps/Step4Address.jsx
@@ -0,0 +1,13 @@
+function Step4Address() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Step 4 - Address Details
+      </h2>
+
+      <p>Address form here</p>
+    </div>
+  );
+}
+
+export default Step4Address;
