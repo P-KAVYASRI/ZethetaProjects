a66c329cec9ce2955fa73034c62a2f30c2979870
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-15 13:11:24 +0530

    step7Documents

---
+++ loan-application/src/components/steps/Step7Documents.jsx
@@ -0,0 +1,13 @@
+function Step7Documents() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Step 7 - Documents Upload
+      </h2>
+
+      <p>Document upload form here</p>
+    </div>
+  );
+}
+
+export default Step7Documents;
