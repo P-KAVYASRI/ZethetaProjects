a5dd461ce4d37238e95b1178593bd433a2406587
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-15 13:07:15 +0530

    step2loanPersonalInfo

---
+++ loan-application/src/components/steps/Step2PersonalInfo.jsx
@@ -0,0 +1,13 @@
+function Step2PersonalInfo() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Step 2 - Personal Information
+      </h2>
+
+      <p>Personal information form here</p>
+    </div>
+  );
+}
+
+export default Step2PersonalInfo;
