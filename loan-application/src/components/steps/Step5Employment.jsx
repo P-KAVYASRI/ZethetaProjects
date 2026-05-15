1b353faf02e8da3f2998b199f82faaa34cc3152b
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-15 13:10:01 +0530

    step5Employment

---
+++ loan-application/src/components/steps/Step5Employment.jsx
@@ -0,0 +1,13 @@
+function Step5Employment() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Step 5 - Employment Details
+      </h2>
+
+      <p>Employment form here</p>
+    </div>
+  );
+}
+
+export default Step5Employment;
